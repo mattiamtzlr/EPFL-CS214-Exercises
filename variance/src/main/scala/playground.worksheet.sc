480b5c733b76ed44a38d1ecb56bcd6757c5fdc39
Mattia <mattia.metzler@gmail.com>
2024-10-09 17:53:34 +0200

    EX5_Scala: Variance | DONE | Finished all exercises
    Some of them are haphazardly done but hey

--- variance/src/main/scala/playground.worksheet.sc
+++ variance/src/main/scala/playground.worksheet.sc
@@ -1,1 +1,22 @@
 import variance.*
+
+val fruits = "apples" :: "bananas" :: "cherries" :: "grapes" :: "lemons" :: "oranges" :: "strawberries" :: Nil
+val shapes = "circles" :: "disks" :: "ovals" :: "polygons" :: "squares" :: "triangles" :: Nil
+val names  = "arthur" :: "annika" :: "carla" :: "hannah" :: "leo" :: "leo" :: "olivia" :: "mattia" :: Nil
+
+val fruitStack = ListStack(fruits)
+val shapeStack = ListStack(shapes)
+val nameStack  = ListStack(names)
+
+fruitStack.peek()
+val (popped1, rest1) = fruitStack.pop()
+val (popped2, rest2) = rest1.pop()
+
+val pushed = fruitStack.push("watermelons")
+val (pushPopped, pushRest) = pushed.pop()
+pushRest == fruitStack
+
+val joined = joinStacks(
+    fruitStack :: shapeStack :: nameStack :: Nil
+)
+
--- variance/src/main/scala/variance/Variance.scala
+++ variance/src/main/scala/variance/Variance.scala
@@ -1,36 +1,36 @@
 package variance
 
-trait Stack[T]:
+trait Stack[+T] extends HasTop[T]:
   /** Peek at the top of this stack */
   def peek(): Option[T]
 
   /** Create a new stack with one more entry, at the top */
-  def push(t: T): Stack[T]
+  def push[S >: T](s: S): Stack[S]
+
   /** Separate the top entry from the rest of the stack */
   def pop(): (Option[T], Stack[T])
 
+  def top: Option[T] = peek()
 
 /**
   * Stack implementation using a List to hold the elements
   */
 case class ListStack[T](l: List[T]) extends Stack[T]:
   def peek(): Option[T] = l.headOption
-  def push(t: T): Stack[T] = ListStack(t :: l)
+  def push[S >: T](s: S): Stack[S] = ListStack(s :: l)
   def pop(): (Option[T], Stack[T]) = l match
     case Nil    => (None, ListStack(Nil))
     case h :: t => (Option(h), ListStack(t))
 
 def joinStacks[T](l: List[Stack[T]]): Stack[T] =
-  ???
+  ListStack(l.flatMap(_.peek()))
 
-def mkStackInt(): Stack[Int] =
-  ???
+def mkStackInt(): Stack[Int] = ListStack(List(1, 2, 3))
 
-def mkStackString(): Stack[String] =
-  ???
+def mkStackString(): Stack[String] = ListStack(List("a", "b", "c"))
 
 // Does this work?
-// val tops = joinStacks(List(mkStackInt(), mkStackString()))
+// val tops = joinStacks(List(mkStackInt(), mkStackString())) - nope
 
 trait Drawer[T]:
   def get(): T
@@ -41,14 +41,17 @@ case class IncrementingDrawer(i: Int) extends Drawer[Int]:
   def put(j: Int) = IncrementingDrawer(j + 1)
 
 
-trait Box[T]:
+trait Box[+T] extends HasTop[T]:
   /** Peek at the value inside the box */
   def unbox(): Option[T]
 
   /** Create a new box with the contents */
-  def replace(t: T): Box[T]
+  def replace[S >: T](s: S): Box[S]
+
   /** Create a new box by applying `f` to the contents of this one */
   def map[T2](f: T => T2): Box[T2]
+
+  def top: Option[T] = unbox()
 
 trait HasTop[+T]:
   /** Peek at one value inside this container */
@@ -56,4 +59,4 @@ trait HasTop[+T]:
 
 
 def joinTops[T](l: List[HasTop[T]]): List[T] =
-  ???
+  l.flatMap(_.top)
