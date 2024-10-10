cd33c5ef04cd834f9ce5c92c1baaaaeac782b473
Mattia <mattia.metzler@gmail.com>
2024-10-10 18:43:55 +0200

    EX5_Scala: Comprehensions | DONE | Finished most of the exercises

--- comprehensions/src/main/scala/comprehensions/recursion.scala
+++ comprehensions/src/main/scala/comprehensions/recursion.scala
@@ -1,22 +1,44 @@
 package comprehensions
 
+import scala.annotation.tailrec
+
 def factorial(n: Int): Int =
-  ???
+  require(n >= 0)
+  @tailrec
+  def tailRec(n: Int, acc: Int): Int =
+    if n <= 1 then acc
+    else tailRec(n - 1, n * acc)
+  tailRec(n, 1)
 
-def fastExp(base: Int, exp: Int): Int =
-  ???
+def fastExp(base: Int, exp: Int): Int = exp match
+  case 0 => 1
+  case 1 => base
+  case x => if x % 2 == 0 
+    then fastExp(base * base, exp / 2) 
+    else base * fastExp(base, exp - 1)
 
-def merge(xs: List[Int], ys: List[Int]): List[Int] =
-  ???
+def merge(xs: List[Int], ys: List[Int]): List[Int] = (xs, ys) match
+  case (Nil, _)           => ys
+  case (_, Nil)           => xs
+  case (x :: xt, y :: yt) => if x < y then x :: merge(xt, ys) else y :: merge(xs, yt)
+  // if x < y then merge the tail of x with the whole of y and vice versa
 
-def split[A](l: List[A]): (List[A], List[A]) =
-  ???
+def split[A](l: List[A]): (List[A], List[A]) = l.splitAt(l.length / 2)
 
-def mergeSort(xs: List[Int]): List[Int] =
-    ???
+def mergeSort(xs: List[Int]): List[Int] = xs match
+  case Nil      => xs
+  case x :: Nil => xs
+  case _        => 
+    val (left, right) = split(xs)
+    merge(mergeSort(left), mergeSort(right))
 
+@tailrec
 def decimalToBaseN(number: Int, base: Int, acc: List[Int] = Nil): List[Int] =
-    ???
+  if number < base then number :: acc
+  else decimalToBaseN(number / base, base, (number % base) :: acc)
 
 def coinChange(coins: List[Int], amount: Int): Int =
-    ???
+  if amount == 0 then 1
+  else if amount < 0 || coins.isEmpty then 0
+  else
+    coinChange(coins.tail, amount) + coinChange(coins, amount - coins.head)
---
+++ comprehensions/src/main/scala/playground.worksheet.sc
@@ -0,0 +1,43 @@
+import comprehensions.*
+import Glob.*
+
+val p1 = "users.json"
+val p2 = "20??-??-??.jpg"
+val p3 = "icon.*"
+val p4 = "20??-*.jp*g"
+val p5 = "*"
+
+glob(p1, "users.json")
+glob(p1, "users.csv")
+
+glob(p2, "2024-10-09.jpg")
+glob(p2, "2024-10-09.jpeg")
+
+glob(p3, "icon.png")
+glob(p3, "icon.jpg")
+
+glob(p4, "2024-cat.jpeg")
+glob(p4, "2002-birthday.jpg")
+
+glob(p5, "epfl")
+glob(p5, "insane")
+
+
+factorial(12)
+factorial(19)
+
+fastExp(2, 3)
+fastExp(5, 10)
+
+decimalToBaseN(11, 2).mkString
+decimalToBaseN(7482, 2).mkString
+decimalToBaseN(57, 16).mkString
+
+val unordered = 4 :: 2 :: -3 :: 9 :: 7 :: 1 :: -5 :: Nil
+split(unordered)
+
+val ordered1 = -3 :: 2 :: 4 :: Nil
+val ordered2 = -5 :: 1 :: 7 :: 9 :: Nil
+merge(ordered1, ordered2)
+
+mergeSort(unordered)
