3402294df13f7d5ffd1e68d267ed130165bb3ce8
Mattia <mattia.metzler@gmail.com>
2024-10-15 10:58:15 +0200

    EX6_Scala: Parallelism | WIP | Finished exercise about parMap

--- parallelism/src/main/scala/parallelism/MapReduce.scala
+++ parallelism/src/main/scala/parallelism/MapReduce.scala
@@ -1,11 +1,27 @@
 package parallelism
 
-import scala.collection.parallel.CollectionConverters.*
-
-class MapThread[A, B](seq: Seq[A], a: A, f: A => B) extends Thread:
-  override def run(): Unit = ???
-
+import scala.concurrent.*
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.*
 
 extension [A](seq: Seq[A])
-  def parMap[B](f: A => B): Seq[B] =
-    ???
+  // version 1: using Threads
+  def parMapThread[B](f: A => B): Seq[B] =
+    val results = new Array[Any](seq.length)
+    val threads = seq.zipWithIndex.map {
+      case (a, i) => new Thread(
+        () => {
+          val res = f(a)
+          results.synchronized {
+            results(i) = res
+          }
+        })
+    }
+    threads.foreach(_.start())
+    threads.foreach(_.join())
+    results.asInstanceOf[Array[B]].toSeq
+
+  // version 2: using Future
+  def parMapFuture[B](f: A => B): Seq[B] =
+    val futures: Seq[Future[B]] = seq.map(a => Future {f(a)})
+    futures.map(Await.result(_, Duration.Inf))
--- parallelism/src/main/scala/playground.worksheet.sc
+++ parallelism/src/main/scala/playground.worksheet.sc
@@ -1,1 +1,10 @@
 import parallelism.*
+
+val numbers = List(2, 42, -3, 7, -8)
+val names   = List("Mattia", "Hannah", "Leo", "Carla")
+
+numbers.parMapThread(_ * 2)
+names.parMapThread(_.length())
+
+numbers.parMapFuture(_ * 2)
+names.parMapFuture(_.length())
